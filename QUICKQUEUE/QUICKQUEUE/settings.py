93a582bc0f22720bff4a3c88547911c78f58ab50
fncyz <eurielvinz0718@gmail.com>
2026-09-22 17:09:41 +0800

    Fix settings imports

--- QUICKQUEUE/QUICKQUEUE/settings.py
+++ QUICKQUEUE/QUICKQUEUE/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,8 +83,6 @@ WSGI_APPLICATION = 'QUICKQUEUE.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-import os
-import dj_database_url
 
 DATABASES = {
     'default': dj_database_url.config(
