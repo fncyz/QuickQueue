3643359dbc4e73003413bb6066bfa41b2dda3678
fncyz <eurielvinz0718@gmail.com>
2026-09-22 16:56:07 +0800

    Configure Supabase PostgreSQL for deployment

--- QUICKQUEUE/QUICKQUEUE/settings.py
+++ QUICKQUEUE/QUICKQUEUE/settings.py
@@ -81,15 +81,15 @@ WSGI_APPLICATION = 'QUICKQUEUE.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import os
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'quickqueue',
-        'USER': 'postgres',
-        'PASSWORD': 'quickqueue',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=0,
+        ssl_require=True,
+    )
 }
 
 
