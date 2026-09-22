c83d837db0fbfd439e244f76344355d92bbd8158
fncyz <eurielvinz0718@gmail.com>
2026-09-22 20:41:01 +0800

    Updated EAS build

--- quickqueue-mobile/services/api.ts
+++ quickqueue-mobile/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 export const api = axios.create({
-  baseURL: "http://192.168.254.100:8000/api/",
+  baseURL: "https://quick-queue-virid.vercel.app/api/",
   headers: {
     "Content-Type": "application/json",
   },
